9d8af93abd4a209302ba44ac04f98b5bc12e5971
Петр Горюнов <petr.goryunov.89@ya.ru>
2023-01-11 20:18:30 +0500

    Наполнение проекта шаблонами

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('posts.urls')),
+    path('index.html', include('posts.urls')),
     path('group/<slug:slug>/', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
