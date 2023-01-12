8213597cbaaa8498262ca84ab4aa9f15076bbdb9
Петр Горюнов <petr.goryunov.89@ya.ru>
2023-01-12 19:34:59 +0500

    Структурирование шаблонов

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,18 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Главная страница
 def index(request):
     template = os.path.join(BASE_DIR, 'templates', 'posts', 'index.html')
-    # template = 'posts\index.html'
     return render(request, template)
 
 
 # Посты, отфильтрованные по группам
 def group_posts(request, slug):
-    return HttpResponse(f'Posts by groups: <h1>{slug}</h1>')
+    template = os.path.join(BASE_DIR, 'templates', 'posts', 'group_list.html')
+    return render(request, template)
