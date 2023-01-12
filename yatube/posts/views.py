836434d1e42568e8c890c25a6a8ffa8f94539dfc
Петр Горюнов <petr.goryunov.89@ya.ru>
2023-01-12 20:12:05 +0500

    Передача словарей в функции render

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -9,10 +9,22 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Главная страница
 def index(request):
     template = os.path.join(BASE_DIR, 'templates', 'posts', 'index.html')
-    return render(request, template)
+    title = 'Последние обновления на сайте'
+    content = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'content': content,
+    }
+    return render(request, template, context)
 
 
 # Посты, отфильтрованные по группам
 def group_posts(request, slug):
     template = os.path.join(BASE_DIR, 'templates', 'posts', 'group_list.html')
-    return render(request, template)
+    title = 'Лев Толстой – зеркало русской революции.'
+    content = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'content': content,
+    }
+    return render(request, template, context)
