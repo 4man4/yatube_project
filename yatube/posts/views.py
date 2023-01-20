3e160caac46f2b8c127987fdb1d0687a13911abe
Петр Горюнов <petr.goryunov.89@ya.ru>
2023-01-21 00:34:37 +0500

    Реализация функционала публикации постов и их групп

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,17 @@
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 import os
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Главная страница
 def index(request):
+    """Главная страница."""
     template = os.path.join(BASE_DIR, 'templates', 'posts', 'index.html')
     title = 'Последние обновления на сайте'
-    content = 'Это главная страница проекта Yatube'
+    content = Post.objects.order_by('-pub_date')[:10]
     context = {
         'title': title,
         'content': content,
@@ -18,13 +19,16 @@ def index(request):
     return render(request, template, context)
 
 
-# Посты, отфильтрованные по группам
 def group_posts(request, slug):
+    """Посты, отфильтрованные по группам."""
     template = os.path.join(BASE_DIR, 'templates', 'posts', 'group_list.html')
-    title = 'Лев Толстой – зеркало русской революции.'
-    content = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества {group.title}.'
+    content = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
+        'group': group,
         'title': title,
         'content': content,
     }
+
     return render(request, template, context)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -33,13 +33,13 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Application definition
 
 INSTALLED_APPS = [
-    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
